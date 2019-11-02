922ebc4c02aa893eefd54ee3118aa846e912a793
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2019-11-02 18:35:30 +0900

    comment

--- consts.py
+++ consts.py
@@ -1,9 +1,16 @@
 # coding: utf-8
 
+# GPIO pin number(BCM) of speaker
+SOUND_PIN = 21
+
+# the frequency of "pi!"
 f = 750
 
+# the length of "pi!"
 length = 0.02
+
+# 2 ^ (1/12)
 r = 1.059
-SOUND_PIN = 21
+
 # How many scales to shift in Power Up
 pun = 1
